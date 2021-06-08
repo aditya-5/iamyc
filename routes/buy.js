d131ca69cedd9cb8caf58e60ff0bde62bf71d078
Aditya Agarwal <aditya.7@outlook.com>
2021-06-08 20:36:58 +0530

    Gallery upload

--- routes/buy.js
+++ routes/buy.js
@@ -56,7 +56,7 @@ router.post('/:id/capture/', async function handleRequest(req, res) {
   }
 
   const output= `
-  <p> Thank you for buying the IAM Youth Committee membership. We are delighted to have you on board. </p>
+  <p> Thank you for buying the VisittUs membership. We are delighted to have you on board. </p>
   <p> Your order details are as follows - </p>
   <table border=1>
   <tr> <td> Order ID</td><td> ${capture.result.id} </td></tr>
@@ -64,17 +64,17 @@ router.post('/:id/capture/', async function handleRequest(req, res) {
   <tr> <td> Name</td><td> ${capture.result.payer.name.given_name} ${capture.result.payer.name.surname} </td></tr>
   <tr> <td> Amount Paid</td><td> ${capture.result.purchase_units[0].payments.captures[0].amount.currency_code} ${capture.result.purchase_units[0].payments.captures[0].amount.value} </td></tr>
 
-  <tr> <td> Items Purchased</td><td> IAMYC Membership </td></tr>
+  <tr> <td> Items Purchased</td><td> VisittUs Membership </td></tr>
   </table>
   <p>We will be in touch with you with your membership details within 24-48 hours.
   Looking forward to serving you with the best deals in town.  </p>
   <font color="red"> <p>Important : Please reply to this email and send us a picture of yourself in which your face is distinctly visible.
-  This is intended for verification purposes for whenever you visit one of our partner restaurants.</p></font>
+  This is intended for verification purposes for whenever you visit one of our partner brands.</p></font>
   <br>
   Regards
   <br>
-  Team IAMYC
-  <img  src="cid:iam">
+  Team VisittUs
+  <img  src="cid:iam" style="width:550px;height:200px">
   `
 
   let transporter = nodemailer.createTransport({
@@ -93,18 +93,18 @@ router.post('/:id/capture/', async function handleRequest(req, res) {
 
 // send mail with defined transport object
 let info = transporter.sendMail({
-  from: '"IAM Youth Committee" <contact@adityagarwal.co>', // sender address
+  from: '"VisittUs" <contact@adityagarwal.co>', // sender address
   to: `thats.adi.007@Gmail.com, ${capture.result.payer.email_address}`, // list of receivers
-  subject: "Order Confirmation: IAMYC Membership", // Subject line
+  subject: "Order Confirmation: Visitt.Us Membership", // Subject line
   text: "", // plain text body
   html: output, // html body
   attachments: [{
-          filename: 'iam.png',
-          path: __dirname +'/../public/iam.png',
+          filename: 'visitt.png',
+          path: __dirname +'/../public/visitt.png',
           cid: 'iam'
       }],
 });
-
+console.log(info)
 
 
 
